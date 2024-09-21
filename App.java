b0da367d22256d14d778949d59c8c43650f58f01
sameer-codes-ai <sameer.kumar2024@vitstudent.ac.in>
2024-09-21 15:53:29 +0530

    Update App.java
    Implementing flappyBird2 instead of fappyBird as the former one have the level and background changing capabilities

--- App.java
+++ App.java
@@ -12,7 +12,7 @@ public class App {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        FlappyBird2 flappyBird = new FlappyBird2();
+        FlappyBird2 flappyBird2 = new FlappyBird2();
         frame.add(flappyBird);
         frame.pack();
         flappyBird.requestFocus();
